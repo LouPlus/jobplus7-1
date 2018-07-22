ed3818b2df9a729f37c3ec24334be3f900af159e
用笨方法写三遍 <ux23@qq.com>
2018-07-22 15:39:56 +0800

    update manage.py app.py config.py

---
+++ jobplus/handlers/front.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+
+front = Blueprint('front', __name__, url_prefix='/')
+
---
+++ jobplus/handlers/jobs.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+
+
+jobs = ('jobs', __name__, url_prefix='/jobs')
+
