4cc75e3d9f31d1860769911d3c993a73520004bf
enjoyphen <39270410+enjoyphen@users.noreply.github.com>
2018-07-23 10:08:31 +0800

    edit the base.html and macros.html (#14)
    底部栏需要修改.

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,5 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+from flask_login import login_required
 
 
 front = Blueprint('front', __name__, url_prefix='/')
 
+@front.route('/')
+def index():
+    return render_template('index.html')
+
+@front.route('/login')
+def login():
+    return '登录'
+
+@front.route('/register')
+def register():
+    return '用户注册'
+
+@front.route('/logout')
+@login_required
+def logout():
+    return '退出'
+    
