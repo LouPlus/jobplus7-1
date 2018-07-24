2e53b1447a1d46c372fcff21a4c745198086009a
用笨方法写三遍 <ux23@qq.com>
2018-07-24 23:47:42 +0800

    make homepage more dtails

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,13 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
 from flask_login import login_required
+from jobplus.models import db, Company, Job
 
 
 front = Blueprint('front', __name__, url_prefix='/')
 
 @front.route('/')
 def index():
-    return render_template('index.html')
+    jobs = Job.query.paginate(
+            page=1,
+            per_page=current_app.config['INDEX_PER_PAGE'],
+            error_out=True
+            )
+    companies = Company.query.paginate(
+            page=1,
+            per_page=current_app.config['INDEX_PER_PAGE'],
+            error_out=True
+            )
+    return render_template('index.html', jobs=jobs, companies=companies, Company=Company)
 
 @front.route('/login')
 def login():
