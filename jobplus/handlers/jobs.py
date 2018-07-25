7075d290e79f323b7ba1af202a4cd0dc5aa1cb0a
ChuJune <zy.de@outlook.com>
2018-07-25 23:55:31 +0800

    添加job详情页，修改models.py(添加描述、领域） (#42)
    添加job详情页,修改models.py

--- jobplus/handlers/jobs.py
+++ jobplus/handlers/jobs.py
@@ -1,5 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from jobplus.models import Job
 
+job = Blueprint('job', __name__, url_prefix='/jobs')
 
-jobs = Blueprint('jobs', __name__, url_prefix='/jobs')
+@job.route('/<int:job_id>')
+def detail(job_id):
+    job = Job.query.get_or_404(job_id)
+    return render_template('company/job_detail.html', job=job)
+
 
