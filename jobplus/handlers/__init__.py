fab88af8ad21b55c9b68f55b13199a41bf9e3e0f
用笨方法写三遍 <ux23@qq.com>
2018-07-26 16:36:48 +0800

    暂时提交

--- jobplus/handlers/__init__.py
+++ jobplus/handlers/__init__.py
@@ -1,6 +1,6 @@
 from .admin import admin
 from .company import company
 from .front import front
-from .jobs import jobs
+from .jobs import job
 from .user import user
 
--- jobplus/handlers/user.py
+++ jobplus/handlers/user.py
@@ -1,8 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from werkzeug import secure_filename
+from flask_login import login_required
+from jobplus.forms import PersonalForm
+from jobplus.models import Personal
+
 
 
 user = Blueprint('user', __name__, url_prefix='/user')
 
-@user.route('/')
-def index():
-    return '用户信息配置页面'
+@user.route('/profile', methods=['POST','GET'])
+@login_required
+def profile():
+    form = PersonalForm()
+    if form.validate_on_submit():
+        person = Personal()
+    
+    return render_template('user/profile.html',form=form)
+
