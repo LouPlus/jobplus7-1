c0343d1ccb2cd234e1b5cc907438a570288716f7
zyong <zy.de@outlook.com>
2018-07-26 14:09:08 +0800

    models拼写错误, add_data添加融资、领域数据

--- jobplus/models.py
+++ jobplus/models.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 class Base(db.Model):
     __abstract__ = True
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.Datetime,
+    updated_at = db.Column(db.DateTime,
                         default=datetime.utcnow,
                         onupdate=datetime.utcnow)
 
