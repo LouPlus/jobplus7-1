b5ef0531f66b63da7dd24305394c4c5d6d442c0b
用苯方法写三遍 <ux23@qq.com>
2018-07-28 12:10:53 +0800

    统一为视图job

--- jobplus/handlers/jobs.py
+++ jobplus/handlers/jobs.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
 from jobplus.models import Job
 
-job = Blueprint('job', __name__, url_prefix='/jobs')
+jobs = Blueprint('job', __name__, url_prefix='/jobs')
 
-@job.route('/<int:job_id>')
+@jobs.route('/<int:job_id>')
 def detail(job_id):
     job = Job.query.get_or_404(job_id)
     return render_template('company/job_detail.html', job=job)
